e9e94f286857304a41f040d7f9591e954a7eabb9
Alex-Buttielie <buttielle3@gmail.com>
2022-03-11 09:23:05 -0300

    primeira atividade "Hellow world"

--- curso-udemy/bar/src/main/java/br/com/erudio/GreetingController.java
+++ curso-udemy/bar/src/main/java/br/com/erudio/GreetingController.java
@@ -1,2 +1,19 @@
-package br.com.erudio;public class GreetingController {
+package br.com.erudio;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GreetingController {
+    private static final String template = "Hello, %s";
+    private  final AtomicLong count = new AtomicLong();
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public Greeting greeting (@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(count.incrementAndGet(), String.format(template, name));
+    }
+
 }
